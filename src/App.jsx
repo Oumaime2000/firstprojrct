29ad7116884fc473bd166ae74a6f44f7bcda837f
oumaima <cherifoumaima215@gmail.com>
2023-03-09 12:48:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='navbar'>
+          <div>
+            <h3 className='logo' >Wakelni</h3>
+          </div>
+          <div className='navteams'>
+            <h3>about us </h3>
+            <h3>best seller </h3>
+            <h3>contact us </h3>
+
+          </div>
+        </div>
+        <div className='adresstitle'>
+        <h4 className='adress'> Adresse : Av. Elissa, Jardins de carthage 1090 </h4>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        facebook
+        </a>
+      <h5 className='title' >eat at Wakelni</h5>
+      </div>
+    </header> 
+      <div>
+        <h1>Raoui</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
